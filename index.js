90e431561eef20e080f66c47225e7c838f1b8fe6
ToiNguyen <minhtoi2799@gmail.com>
2020-10-09 15:33:05 +0700

    init keystone project

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { Text } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { StaticApp } = require('@keystonejs/app-static');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'cms-university-reviews';
+const adapterConfig = { mongoUri: 'mongodb://127.0.0.1:27017/cms-university-reviews' };
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+keystone.createList('Todo', {
+  schemaDoc: 'A list of things which need to be done',
+  fields: {
+    name: { type: Text, schemaDoc: 'This is the thing you need to do' },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new StaticApp({ path: '/', src: 'public' }),
+    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+  ],
+};
